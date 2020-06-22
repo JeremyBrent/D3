5ecbcbbbab779ab65bc688cd1e0a07ec2554576d
JeremyBrent <jeremybrentdata@gmail.com>
2020-06-22 00:41:19 -0400

    added background grid, centered text in scatter dots, etc

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -3,12 +3,12 @@ var xAxisName = "Poverty"
 var yAxisName = "Obesity"
 
 
-var svgHeight = 550;
-var svgWidth = 550;
+var svgHeight = 750;
+var svgWidth = 750;
 
 var margin = {
-    top: 40,
-    right: 20,
+    top: 75,
+    right: 50,
     left: 50,
     bottom: 50
 };
@@ -18,36 +18,40 @@ var chartHeight = svgHeight - margin.top - margin.bottom;
 
 var svg = d3.select("#scatter")
     .append('svg')
+    .classed("chart",true)
     .attr("width", svgHeight)
     .attr('height', svgHeight);
 
 var chartCanvass = svg.append('g')
+    
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Setting Chart title
 chartCanvass.append("g").append("text")
-    .attr("x", (chartWidth / 2))
+    .attr("x", (margin.left))
     .attr("y", .75 - (margin.top / 2))
-    .attr("text-anchor", "middle")
-    .style("font-size", "16px")
-    .style("text-decoration", "underline")
+    .attr("text-anchor", "left")
+    .style("font-size", "30px")
+    .style('font-family',"sans-serif")
     .text(`${xAxisName} vs ${yAxisName}`);
 
-// Setting x axis label
+// Setting y axis label
 chartCanvass.append('g').append('text')
     .attr("x", 0 - (chartHeight / 2))
     .attr('y', 0 - (margin.left))
     .attr("dy", "1em")
     .style("text-anchor", "middle")
     .attr("transform", "rotate(-90)")
+    .style('font-family',"sans-serif")
     .text(`${yAxisName} (% of pop.)`);
 
-// Setting y axis label
+// Setting x axis label
 chartCanvass.append('g').append('text')
     .attr("transform",
         "translate(" + (chartWidth / 2) + " ," +
-        (chartHeight + margin.top) + ")")
+        (chartHeight + margin.top - 35) + ")")
     .style("text-anchor", "middle")
+    .style('font-family',"sans-serif")
     .text(`${xAxisName} (% of pop.)`);
 
 
@@ -88,38 +92,40 @@ d3.csv('assets/data/data.csv').then(function (data) {
     var toolTip = d3.select("body").append("div")
         .attr("class", "tooltip d3-tip");
 
-    /// Creating the scatter points
+    /// Creating the chart dots variable
     var chartDots = chartCanvass.selectAll("circle")
         .data(data)
         .enter()
         .append("g");
 
+    // Adding scatter dot locations
     chartDots.append("circle")
         .classed("stateCircle", true)
         .attr("cx", d => xScale(d.poverty))
         .attr("cy", d => yScale(d.obesity))
-        .attr("r", 8)
-        // Tool tip 
-        .on("mouseover", function (d) {
-            toolTip.transition()
-                .duration(1000)
-                .style("display", "block")
-                .style("opacity", 1);
-            toolTip.html(`<strong>State:</strong> ${d.state} <br> 
-                <strong>${xAxisName}:</strong> ${d.poverty}%<br> 
-                <strong>${yAxisName}:</strong> ${d.obesity}%`)
-                .style("left", d3.event.pageX + "px")
-                .style("top", d3.event.pageY + "px")
-        })
-        .on("mouseout", function () {
-            toolTip.style("display", "none")
-        });
-
-    chartDots.append("text")
+        .attr("r", 10)
+        
+    // Adding scatter dot text 
+    chartDots.append("g").append("text")
         .text(d => d.abbr)
         .classed("stateText", true)
         .attr("x", d => xScale(d.poverty))
-        .attr("y", d => yScale(d.obesity));
+        .attr("y", d => yScale(d.obesity) + 3);
+
+    // Adding tool tip 
+    chartDots.on("mouseover", function (d) {
+        toolTip
+            .style("display", "block")
+            .style("opacity", 1);
+        toolTip.html(`<strong>State:</strong> ${d.state} <br> 
+            <strong>${xAxisName}:</strong> ${d.poverty}%<br> 
+            <strong>${yAxisName}:</strong> ${d.obesity}%`)
+            .style("left", d3.event.pageX + "px")
+            .style("top", d3.event.pageY + "px");
+    })
+    .on("mouseout", function () {
+        toolTip.style("display", "none");
+    });
 
 })
 
